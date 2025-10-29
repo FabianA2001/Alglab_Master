cb2652d15cd29f48f8c4c070be58dd6ae320895c
Caroline Lücking <caroline.luecking@tu-braunschweig.de>
2025-10-29 14:00:06 +0100

    shift rotation constraint

---
+++ src/module/shift_rotation_constraint.py
@@ -0,0 +1,25 @@
+from ortools.sat.python import cp_model
+
+from .. import shift_vars
+from ..inputTypes import instace, shiftType, shift
+from .shift_assignment_module import ShiftAssignmentModule
+
+
+class Shift_rotation_constraint(ShiftAssignmentModule):
+    def build(
+        self,
+        instance: instace.Instance,
+        vars: shift_vars.Shift_vars,
+    ) -> cp_model.LinearExprT:
+        for employee_uid in instance.employees:
+            for day in range(instance.number_of_days-1):
+                assigned_shifts = []
+                for type_uid in instance.shifts[day]:
+                    assigned_shifts.append(
+                        vars.vars[(day, type_uid, employee_uid)])
+                    for btype_uid in instance.shift_types[type_uid].blocked_shifts_after:
+                        assigned_shifts.append(
+                            vars.vars[(day+1, btype_uid, employee_uid)])
+                    vars.model.AddAtMostOne(assigned_shifts)
+                    vars.model.AddAtMostOne(assigned_shifts)
+        return 0
