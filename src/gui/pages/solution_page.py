578103e7fe49b8631a95b51d069d7c86c6adca1e
Fabian A <>
2025-10-31 19:58:44 +0100

    draw shift

--- src/gui/pages/solution_page.py
+++ src/gui/pages/solution_page.py
@@ -1,37 +1,33 @@
 import pandas as pd
 import streamlit as st
 
-# Beispiel-Daten
-shift_types = ["Frühschicht", "Spätschicht", "Nachtschicht"]
-days = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
+from ... import solution
 
-# Beispielhafte Mitarbeiterbelegung
-data = {
-    day: [
-        ["Anna", "Ben"],  # Früh
-        ["Clara"],  # Spät
-        ["Daniel", "Elias"],  # Nacht
+
+def soluation_to_dataframe(solution: solution.Solution) -> pd.DataFrame:
+    # Diese Funktion sollte die Lösung in ein DataFrame umwandeln
+    days = [day for day in range(solution.instance.number_of_days)]
+    shift_types = [
+        shift_type.name for shift_type in solution.instance.shift_types.values()
     ]
-    for day in days
-}
-
-df = pd.DataFrame(data, index=shift_types)
+    blank_data = {day: [[] for _ in shift_types] for day in days}
+    df = pd.DataFrame(blank_data, index=shift_types)
+    return df
 
 
 def show():
     st.title("✅ Solution")
     st.write("Zeige die berechnete Lösung an.")
 
-    # # Check if solution exists in session state
-    # if "solution" not in st.session_state:
-    #     st.warning("Keine Lösung verfügbar. Bitte zuerst den Solver ausführen.")
-    #     st.info("Gehe zur Solver-Seite um eine Lösung zu berechnen.")
-    #     return
+    # Check if solution exists in session state
+    if "solution" not in st.session_state:
+        st.warning("Keine Lösung verfügbar. Bitte zuerst den Solver ausführen.")
+        st.info("Gehe zur Solver-Seite um eine Lösung zu berechnen.")
+        return
 
-    # solution = st.session_state["solution"]
+    solution = st.session_state["solution"]
 
-    # st.subheader("Lösung Details")
-    edited_df = st.data_editor(
-        df,
+    st.data_editor(
+        soluation_to_dataframe(solution),
         key="shiftplan",
     )
