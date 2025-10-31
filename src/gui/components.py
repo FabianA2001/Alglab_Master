4bd72aff40df97bb85ed28039d1b122aa5c6ac8e
Fabian A <>
2025-10-31 15:24:46 +0100

    x

--- src/gui/components.py
+++ src/gui/components.py
@@ -1,19 +1,21 @@
 from ..inputTypes import instace
 
 
-def solver_component(result_var="result"):
+def main_solver_component(**kwargs):
     return f"""
 ## Solver
-<|Solve|button|on_action=on_button_click|>
-
+<|Solve|button|on_action=on_button_click|>  
+{instance_info_component(kwargs.get("inst"))}
 ### Result
-<|{{{result_var}}}|text|>
+<|{{solution_result.objective_value if solution_result else "No solution available."}}|text|>
 """
 
-def instance_info_component(path, inst: instace.Instance):
+
+def instance_info_component(inst: instace.Instance | None = None):
+    if inst is None:
+        return "no instance loaded."
     return f"""
 ## Instance Info
-- Path: {path}
 - number of shift typs: {len(inst.shift_types)}
 - number of employees: {len(inst.employees)}
 
--- src/gui/start.py
+++ src/gui/start.py
@@ -2,37 +2,34 @@ from pathlib import Path
 
 from taipy.gui import Gui
 
-from .. import shift_vars, solution, solver
+from .. import shift_vars, solver
 from ..inputTypes import instace
 from ..parseData import parseTXT
-from .components import instance_info_component, solver_component
+from .components import main_solver_component
 
 DEFAULT_PATH = Path.joinpath(
-        Path(__file__).resolve().parent.parent.parent, "data", "Instance1.txt"
-    )
+    Path(__file__).resolve().parent.parent.parent, "data", "Instance1.txt"
+)
+
 
 def start_gui():
     inst: instace.Instance = parseTXT.parse_txt(DEFAULT_PATH)
     solver_instance = solver.Solver(inst, shift_vars.Shift_vars(inst))
-    result = "noch nicht gesolved"
-    
+
+    # Initialisiere als None für Taipy State
+    solution_result = None
+
+    def show_solution_text(state):
+        if state.solution_result is None:
+            return "No solution available."
+        else:
+            return f"Solution found with objective value: {state.solution_result.objective_value}"
+
     # Hauptseite mit Modulen
-    page = f"""
-# Alglab Master GUI
+    page = main_solver_component(inst=inst)
 
-{instance_info_component(DEFAULT_PATH, inst)}
-
-{solver_component()}
-"""
-    
     def on_button_click(state):
-        solution_result = solver_instance.solve()
-        state.result = f"Solved! Ergebnis: {solution_result}"
-    
+        state.solution_result = solver_instance.solve()
+
     gui = Gui(page)
-    gui.run(
-        title="Alglab Master",
-        debug=True,
-        dark_mode=False,
-        use_reloader=True
-    )
+    gui.run(title="Alglab Master", debug=True, dark_mode=False, use_reloader=True)
