d5bf7b9dec238eb4dcefc1cdaf4695bee26eb958
Fabian A <>
2025-11-22 14:18:25 +0100

    feat: add test for Solver_for_window

--- tests/test_solver_for_windows.py
+++ tests/test_solver_for_windows.py
@@ -60,3 +60,103 @@ def test_add_end_maximum_consecutive_shifts_constraints():
                 solv.vars.get_var(day, lokal_shift_types[0].uid, lokal_employee.uid)
                 == 1
             )
+
+
+def test_add_start_minimum_consecutive_shifts_constraints():
+    lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
+    lokal_employee = employee.Employee()
+    instance = instace.Instance.create(
+        number_of_days=5,
+        shift_typs=lokal_shift_types,
+        emplyees=[lokal_employee],
+    )
+
+    with AssertModelInfeasible() as model:
+        solv = Solver_for_window(
+            instance,
+            Shift_vars(instance, model=model),
+        )
+        solv.add_start_minimum_consecutive_shifts_constraints(lokal_employee.uid, 3)
+        # Erzwinge eine Schicht an Tag 0, aber keine an Tag 1
+        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende)
+        model.Add(
+            solv.vars.get_var(0, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+        )
+        model.Add(
+            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+        )
+
+
+def test_add_end_minimum_consecutive_shifts_constraints():
+    lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
+    lokal_employee = employee.Employee()
+    instance = instace.Instance.create(
+        number_of_days=5,
+        shift_typs=lokal_shift_types,
+        emplyees=[lokal_employee],
+    )
+
+    with AssertModelInfeasible() as model:
+        solv = Solver_for_window(
+            instance,
+            Shift_vars(instance, model=model),
+        )
+        solv.add_end_minimum_consecutive_shifts_constraints(lokal_employee.uid, 3)
+        # Erzwinge eine Schicht an Tag 4 (letzter Tag), aber keine an Tag 3
+        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende am Ende)
+        model.Add(
+            solv.vars.get_var(4, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+        )
+        model.Add(
+            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+        )
+
+
+def test_add_start_minimum_consecutive_days_off_constraints():
+    lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
+    lokal_employee = employee.Employee()
+    instance = instace.Instance.create(
+        number_of_days=5,
+        shift_typs=lokal_shift_types,
+        emplyees=[lokal_employee],
+    )
+
+    with AssertModelInfeasible() as model:
+        solv = Solver_for_window(
+            instance,
+            Shift_vars(instance, model=model),
+        )
+        solv.add_start_minimum_consecutive_days_off_constraints(lokal_employee.uid, 3)
+        # Erzwinge keinen Dienst an Tag 0 (frei), aber Dienst an Tag 1
+        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende freie Tage)
+        model.Add(
+            solv.vars.get_var(0, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+        )
+        model.Add(
+            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+        )
+
+
+def test_add_end_minimum_consecutive_days_off_constraints():
+    lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
+    lokal_employee = employee.Employee()
+    instance = instace.Instance.create(
+        number_of_days=5,
+        shift_typs=lokal_shift_types,
+        emplyees=[lokal_employee],
+    )
+
+    with AssertModelInfeasible() as model:
+        solv = Solver_for_window(
+            instance,
+            Shift_vars(instance, model=model),
+        )
+        solv.add_end_minimum_consecutive_days_off_constraints(lokal_employee.uid, 3)
+        # Erzwinge keinen Dienst an Tag 4 (letzter Tag frei), aber Dienst an Tag 3
+        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende freie Tage am Ende)
+        model.Add(
+            solv.vars.get_var(4, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+        )
+        model.Add(
+            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+        )
