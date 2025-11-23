0917098deefeafce0f0a7bfa8dfbb59798f205f1
Fabian A <>
2025-11-23 13:06:00 +0100

    fix: test days

--- tests/test_solver_for_windows.py
+++ tests/test_solver_for_windows.py
@@ -91,16 +91,25 @@ def test_add_start_minimum_consecutive_shifts_constraints(
         )
         # Erzwinge Schichten gemäß Parametern
         model.Add(
-            solv.vars.get_var(0, lokal_shift_types[0].uid, lokal_employee.uid)
+            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid)
             == day0_value
         )
         model.Add(
-            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid)
+            solv.vars.get_var(2, lokal_shift_types[0].uid, lokal_employee.uid)
             == day1_value
         )
 
 
-def test_add_end_minimum_consecutive_shifts_constraints():
+@pytest.mark.parametrize(
+    "min_consecutive,day3_value,day4_value",
+    [
+        (2, 0, 0),
+        (2, 0, 1),
+    ],
+)
+def test_add_end_minimum_consecutive_shifts_constraints(
+    min_consecutive, day3_value, day4_value
+):
     lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
     lokal_employee = employee.Employee()
     instance = instace.Instance.create(
@@ -114,18 +123,30 @@ def test_add_end_minimum_consecutive_shifts_constraints():
             instance,
             Shift_vars(instance, model=model),
         )
-        solv.add_end_minimum_consecutive_shifts_constraints(lokal_employee.uid, 3)
-        # Erzwinge eine Schicht an Tag 4 (letzter Tag), aber keine an Tag 3
-        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende am Ende)
+        solv.add_end_minimum_consecutive_shifts_constraints(
+            lokal_employee.uid, min_consecutive
+        )
+        # Erzwinge Schichten gemäß Parametern
         model.Add(
-            solv.vars.get_var(4, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+            solv.vars.get_var(2, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day3_value
         )
         model.Add(
-            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day4_value
         )
 
 
-def test_add_start_minimum_consecutive_days_off_constraints():
+@pytest.mark.parametrize(
+    "min_consecutive,day0_value,day1_value",
+    [
+        (2, 1, 1),
+        (2, 1, 0),
+    ],
+)
+def test_add_start_minimum_consecutive_days_off_constraints(
+    min_consecutive, day0_value, day1_value
+):
     lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
     lokal_employee = employee.Employee()
     instance = instace.Instance.create(
@@ -139,18 +160,30 @@ def test_add_start_minimum_consecutive_days_off_constraints():
             instance,
             Shift_vars(instance, model=model),
         )
-        solv.add_start_minimum_consecutive_days_off_constraints(lokal_employee.uid, 3)
-        # Erzwinge keinen Dienst an Tag 0 (frei), aber Dienst an Tag 1
-        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende freie Tage)
+        solv.add_start_minimum_consecutive_days_off_constraints(
+            lokal_employee.uid, min_consecutive
+        )
+        # Erzwinge Schichten gemäß Parametern
         model.Add(
-            solv.vars.get_var(0, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day0_value
         )
         model.Add(
-            solv.vars.get_var(1, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+            solv.vars.get_var(2, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day1_value
         )
 
 
-def test_add_end_minimum_consecutive_days_off_constraints():
+@pytest.mark.parametrize(
+    "min_consecutive,day3_value,day4_value",
+    [
+        (2, 0, 1),
+        (2, 1, 0),
+    ],
+)
+def test_add_end_minimum_consecutive_days_off_constraints(
+    min_consecutive, day3_value, day4_value
+):
     lokal_shift_types = [shiftType.ShiftType() for _ in range(1)]
     lokal_employee = employee.Employee()
     instance = instace.Instance.create(
@@ -164,12 +197,15 @@ def test_add_end_minimum_consecutive_days_off_constraints():
             instance,
             Shift_vars(instance, model=model),
         )
-        solv.add_end_minimum_consecutive_days_off_constraints(lokal_employee.uid, 3)
-        # Erzwinge keinen Dienst an Tag 4 (letzter Tag frei), aber Dienst an Tag 3
-        # Das verletzt die Minimum-Constraint (min 3 aufeinanderfolgende freie Tage am Ende)
+        solv.add_end_minimum_consecutive_days_off_constraints(
+            lokal_employee.uid, min_consecutive
+        )
+        # Erzwinge Schichten gemäß Parametern
         model.Add(
-            solv.vars.get_var(4, lokal_shift_types[0].uid, lokal_employee.uid) == 0
+            solv.vars.get_var(2, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day3_value
         )
         model.Add(
-            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid) == 1
+            solv.vars.get_var(3, lokal_shift_types[0].uid, lokal_employee.uid)
+            == day4_value
         )
