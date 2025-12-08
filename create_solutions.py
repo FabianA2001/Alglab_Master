3360a1fcbfcdbdeb932abc2788e1a87986b03a4c
Almekkawi <Almekkawi>
2025-12-08 10:36:05 +0100

    feat(): Automatic way to create solutions and solutions for instances 1-5 and 10-11

--- create_solutions.py
+++ create_solutions.py
@@ -17,12 +17,17 @@ def main():
     DATA_DIR = Path(__file__).resolve().parent / "data" / "instance_raw"
     json_dir = DATA_DIR
     json_files = list(json_dir.glob("*.txt"))
-
+    dict_constraint = {
+        #0:"original",1:"new",2:"Alternative",3:"Alternative_enforce",4:"Alternative_exact",5:"Alternative_exact_Enforce",
+                       7:"Alternative_exact_original",8:"Alternative_Enforce_If_original"}
     for json_file in json_files:
-        for x in range(2):
-            print(f"Processing {json_file.name} with iteration {x}...")
-            # Call the secondary script with the current JSON file and iteration x
-            subprocess.run(["python", "process_json.py", str(json_file), str(x)])
+        for x in range(0, 3):
+            for constraint_number, key in dict_constraint.items():
+                print(constraint_number, key)
+                print(f"Processing {json_file.name} with iteration {x} key {key}...")
+                # Call the secondary script with the current JSON file and iteration x
+                subprocess.run(["python", "process_json.py", str(json_file), str(x), str(constraint_number), key])
+                subprocess.run(["python", "process_json_immediate.py", str(json_file), str(x), str(constraint_number), key])
 
 if __name__ == "__main__":
     main()
--- process_json_immediate.py
+++ process_json_immediate.py
@@ -14,14 +14,14 @@ from typing import List
 from ortools.sat.python import cp_model
 import gc
 
-def process_file(json_file, x, constraint_number, key):
+def process_file_immediate(json_file, x, constraint_number, key):
     instance = parseTXT.parse_txt(json_file).model_copy(deep=True)
     solver = Solver(instance, Shift_vars(instance))
 
-    filename = f"{instance.name}_{key}_first_good_{x}"  # Include x in the filename
+    filename = f"{instance.name}_{key}_immediate_first_{x}"  # Include x in the filename
     print("\n" + filename)
 
-    solution = solver.solve_with_early_stop(log_search_progress=False, max_time_in_seconds=30*60, constraint_set = constraint_number).model_copy(deep=True)
+    solution = solver.solve_with_early_stop_immediate(log_search_progress=False, max_time_in_seconds=30*60, constraint_set = constraint_number).model_copy(deep=True)
 
     if solution.solve_status in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
         solution.instance.name = filename
@@ -30,9 +30,10 @@ def process_file(json_file, x, constraint_number, key):
         with open("no_solution_found_new.txt", 'a') as file:
             file.write(filename + "\n")
 
+
 if __name__ == "__main__":
     json_file_path = Path(sys.argv[1])
     x = sys.argv[2]  # Get x from command-line arguments
     constraint_number = int(sys.argv[3])
     key = sys.argv[4]
-    process_file(json_file_path, x, constraint_number, key)
+    process_file_immediate(json_file_path, x, constraint_number, key)
