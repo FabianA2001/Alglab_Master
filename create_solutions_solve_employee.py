cfddd1e33bdeb841bbd2bc46892a03c88caaffbd
Almekkawi <Almekkawi>
2025-12-20 21:55:54 +0100

    benchmark

--- create_solutions_solve_employee.py
+++ create_solutions_solve_employee.py
@@ -58,8 +58,8 @@ def main():
     #             print(f"Processing {json_file.name} with iteration {x} ...")
     #             # Call the secondary script with the current JSON file and iteration x
     #             subprocess.run(["python3", "process_json_solve_employee.py", str(json_file), str(x), "True", incrementally, str(30*60), "True"])
-    for one_shift_time, work_var_time, opt_time in [(0, 0, 0),(2.5, 2.5, 0), (2.5, 5, 0), (1, 1, 0), (2.5, 0, 0), (1, 0, 0),  (5, 2.5, 0), (5, 5, 0),  (10, 5, 0), (5, 10, 0),  (10, 10, 0), (0, 0, 30), (0, 2.5, 27.5), (2.5, 0, 27.5), (2.5, 2.5, 25), (2.5, 5, 22.5), (5, 2.5, 22.5), (5, 5, 20), (5, 10, 15), (10, 10, 10), (10, 5, 15)]:
-        for json_file in json_files_best_till_time:
+    for json_file in json_files_best_till_time:
+        for one_shift_time, work_var_time, opt_time in [(0, 0, 0),(2.5, 2.5, 0), (2.5, 5, 0), (1, 1, 0), (2.5, 0, 0), (1, 0, 0),  (5, 2.5, 0), (5, 5, 0),  (10, 5, 0), (5, 10, 0),  (10, 10, 0), (0, 0, 30), (0, 2.5, 27.5), (2.5, 0, 27.5), (2.5, 2.5, 25), (2.5, 5, 22.5), (5, 2.5, 22.5), (5, 5, 20), (5, 10, 15), (10, 10, 10), (10, 5, 15)]:
             for x in range(0, 3):
                 print(f"one_shift, work_var, opt times: {(one_shift_time, work_var_time, opt_time)}")
                 print(f"Processing {json_file.name} with iteration {x} ...")
@@ -77,7 +77,7 @@ def main():
                         filename = f"{solution.instance.name}_1S{one_shift_time}_wv{work_var_time}_o{opt_time}_time_out_30_{x}"
                         solution.to_json_file(filename)
                     else:
-                        error_filename = f"{solution.instance.name}_1S{one_shift_time}_wv{work_var_time}_o{opt_time}"
+                        error_filename = f"{solution.instance.name}_1S{one_shift_time}_wv{work_var_time}_o{opt_time}_{x}"
                         with open('error_log.txt', 'a') as error_file:
                             error_file.write(error_filename + '\n')
                 else:
