f301a9c6a7bdd443b243babc27410b7b7a8ef316
Almekkawi <Almekkawi>
2025-12-23 16:53:40 +0100

    refactor:

--- create_solutions_solve_employee.py
+++ create_solutions_solve_employee.py
@@ -106,7 +106,7 @@ def main():
                     filename = f"{solution.instance.name}_1S{one_shift_time}_wv{work_var_time}_o{opt_time}_{dict_objective_values[json_file.stem]}_immediate_first_{x}"
                     solution.to_json_file(filename)
 
-    for one_shift_time, work_var_time, opt_time in [(10,10,10), (0,0,30)]:
+    for one_shift_time, work_var_time, opt_time in [(10,10,10), (0,0,30), (10,10,0)]:
         # ,  (2.5, 0, 0), (1, 0, 0),  (5, 2.5, 0), (5, 5, 0),  (10, 5, 0), (5, 10, 0),  (10, 10, 0), (0, 0, 30), (0, 2.5, 27.5), (2.5, 0, 27.5), (2.5, 2.5, 25), (2.5, 5, 22.5), (5, 2.5, 22.5), (5, 5, 20), (5, 10, 15), (10, 10, 10), (10, 5, 15)
         for json_file in json_files_best_till_time:
             for x in range(0, 3):
