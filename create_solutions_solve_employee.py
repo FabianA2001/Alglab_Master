61b4dd6f046f7d3d4ad5314b487a6af3dbbbdba6
Almekkawi <Almekkawi>
2026-01-16 20:24:20 +0100

    refactor:

--- create_solutions_solve_employee.py
+++ create_solutions_solve_employee.py
@@ -171,7 +171,7 @@ def main():
                                                         ]:
     #     # ,  (2.5, 0, 0), (1, 0, 0),  (5, 2.5, 0), (5, 5, 0),  (10, 5, 0), (5, 10, 0),  (10, 10, 0), (0, 0, 30), (0, 2.5, 27.5), (2.5, 0, 27.5), (2.5, 2.5, 25), (2.5, 5, 22.5), (5, 2.5, 22.5), (5, 5, 20), (5, 10, 15), (10, 10, 10), (10, 5, 15)
         for json_file in json_files_best_till_time:
-            for x in range(0, 1):
+            for x in range(0, 3):
                 for percentual_improvement_shift, time_between_checks_in_seconds_shift, percentual_improvement_work_var, time_between_checks_in_seconds_work_var, numerical_improvement_opt in [(0.025,8,0.012,8,0)]:
                     time_between_checks_in_seconds_opt=120
                     callback_one_shift = callback_improvement_slowed(percentual_improvement=percentual_improvement_shift, time_between_checks_in_seconds=time_between_checks_in_seconds_shift)
@@ -190,7 +190,7 @@ def main():
                     first_opt_time=time.time()-start_time
 
                     print(opt_time*60-(time.time()-start_time))
-                    for not_better_stop_timer in [30]:
+                    for not_better_stop_timer in [30, 5*60]:
                         lns_start_time = time.time()
                         solution_lns = lns.LNS(sol_or_instance=solution.model_copy(deep=True), timeout_seconds=timeout_seconds, log_level=50).solve(not_better_break_after=not_better_stop_timer)
 
