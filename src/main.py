32c3bedc5c57c756c4b7f7e570d0e40c238c0e9d
Caroline Lücking <caroline.luecking@tu-braunschweig.de>
2026-01-18 20:15:14 +0100

    test merge konflikt

--- src/main.py
+++ src/main.py
@@ -306,9 +306,9 @@ def main() -> None:
     # try_warmstart_callback()
     # calculate_all_instancen()
     # print_some_infos()
-    t_minimal_changes_lns()
+    # test_minimal_changes_lns()
     # test_double_lns()
-    # t_slice_window
+    t_slice_window()
 
 
 if __name__ == "__main__":
